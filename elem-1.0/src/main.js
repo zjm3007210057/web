97eb205bf7d95549d0ace6bfce7b38abf5cf75cb
JianmingZhang <zhang.jianming@opg.cn>
2017-09-06 12:36:08 +0800

    add header

--- elem-1.0/src/main.js
+++ elem-1.0/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import goods from 'components/goods/goods'
 import seller from 'components/seller/seller'
 import ratings from 'components/ratings/ratings'
@@ -8,6 +9,7 @@ import ratings from 'components/ratings/ratings'
 import 'common/stylus/index.styl'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 let app = Vue.extend(App)
 
