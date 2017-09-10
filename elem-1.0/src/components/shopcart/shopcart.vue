dd1e3c14b3d8f279ae98552aed2d1de47003cf25
zjm <zjm3007210057@163.com>
2017-09-10 10:54:17 +0800

    modify shopcart transitions position

--- elem-1.0/src/components/shopcart/shopcart.vue
+++ elem-1.0/src/components/shopcart/shopcart.vue
@@ -112,43 +112,43 @@
             return
           }
         }
-      },
-      transitions: {
-        drop: {
-          beforeEnter(el) {
-            let count = this.balls.length
-            while (count--) {
-              let ball = this.balls[count]
-              if (ball.show) {
-                let rect = ball.el.getBoundingClientRect()
-                let x = rect.left - 32
-                let y = -(window.innerHeight - rect.top -22)
-                el.style.display = ''
-                el.style.webkitTransform = `translate3d(0,${y}px,0)`
-                el.style.transform = `translate3d(0,${y}px,0)`
-                let inner = el.getElementsByClassName('inner-hook')[0]
-                inner.style.webkitTransform = `translate3d(${x}px,0,0)`
-                inner.style.transform = `translate3d(${x}px,0,0)`
-              }
-            }
-          },
-          enter(el) {
-            /* eslint-disable no-unused-vars */
-            let rf = el.offsetHeight
-            this.$nextTick(() => {
-              el.style.webkitTransform = 'translate3d(0,0,0)'
-              el.style.transform = 'translate3d(0,0,0)'
+      }
+    },
+    transitions: {
+      drop: {
+        beforeEnter(el) {
+          let count = this.balls.length
+          while (count--) {
+            let ball = this.balls[count]
+            if (ball.show) {
+              let rect = ball.el.getBoundingClientRect()
+              let x = rect.left - 32
+              let y = -(window.innerHeight - rect.top -22)
+              el.style.display = ''
+              el.style.webkitTransform = `translate3d(0,${y}px,0)`
+              el.style.transform = `translate3d(0,${y}px,0)`
               let inner = el.getElementsByClassName('inner-hook')[0]
-              inner.style.webkitTransform = 'translate3d(0,0,0)'
-              inner.style.transform = 'translate3d(0,0,0)'
-            })
-          },
-          afterEnter(el) {
-            let ball = this.dropBalls.shift()
-            if(ball) {
-              ball.show = false
-              el.style.display = 'none'
+              inner.style.webkitTransform = `translate3d(${x}px,0,0)`
+              inner.style.transform = `translate3d(${x}px,0,0)`
             }
+          }
+        },
+        enter(el) {
+          /* eslint-disable no-unused-vars */
+          let rf = el.offsetHeight
+          this.$nextTick(() => {
+            el.style.webkitTransform = 'translate3d(0,0,0)'
+            el.style.transform = 'translate3d(0,0,0)'
+            let inner = el.getElementsByClassName('inner-hook')[0]
+            inner.style.webkitTransform = 'translate3d(0,0,0)'
+            inner.style.transform = 'translate3d(0,0,0)'
+          })
+        },
+        afterEnter(el) {
+          let ball = this.dropBalls.shift()
+          if(ball) {
+            ball.show = false
+            el.style.display = 'none'
           }
         }
       }
@@ -254,11 +254,11 @@
         bottom: 22px
         z-index: 200
         &.drop-transition
-          transition: all 0.4s
+          transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
           .inner
             width: 16px
             height: 16px
             border-radius: 50%
             background: rgb(0, 160, 220)
-            transition: all 0.4s
+            transition: all 0.4s linear
 </style>
