1084b787bfde7be6a206894f9390fa7c27cc4f87
zjm <zjm3007210057@163.com>
2017-09-11 23:12:00 +0800

    add ratings

--- elem-1.0/src/components/food/food.vue
+++ elem-1.0/src/components/food/food.vue
@@ -11,7 +11,7 @@
         <h1 class="title">{{food.name}}</h1>
         <div class="detail">
           <span class="sell-count">月售{{food.sellCount}}份</span>
-          <span class="rating">好评率{{food.rating}}</span>
+          <span class="rating">好评率{{food.rating}}%</span>
         </div>
         <div class="price">
           <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
@@ -26,6 +26,27 @@
         <h1 class="title">商品信息</h1>
         <p class="text">{{food.info}}</p>
       </div>
+      <split></split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <ratingselect :select-type="selectType" :only-content="onlyContent"
+          :desc="desc" :ratings="food.ratings"></ratingselect>
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+              <div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img class="avatar" width="12" height="12" :src="rating.avatar">
+              </div>
+              <div class="time">{{rating.rateTime}}</div>
+              <p class="text">
+                <span :class="{'icon-thumb_up':rating.rateType===0, 'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+              </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || !food.ratings.length"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +56,9 @@
   import cartcontrol from 'components/cartcontrol/cartcontrol'
   import Vue from 'vue'
   import split from 'components/split/split'
+  import ratingselect from 'components/ratingselect/ratingselect'
+
+  const ALL = 2
 
   export default {
     props: {
@@ -44,12 +68,21 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       }
     },
     methods: {
       show() {
         this.showFlag = true
+        this.selectType = ALL
+        this.onlyContent = true
         this.$nextTick(() => {
           if(!this.scroll) {
             this.scroll = new BScroll(this.$els.food, {
@@ -69,16 +102,37 @@
         }
         this.$dispatch('cart.add', event.target)
         Vue.set(this.food, 'count', 1)
+      },
+      needShow(type, text) {
+        if(this.onlyContent && !text) {
+          return false
+        }
+        if(this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType
+        }
+      }
+    },
+    event: {
+      'ratingtype.select'(type) {
+        this.selectType = type
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent
       }
     },
     components: {
       cartcontrol: cartcontrol,
-      split: split
+      split: split,
+      ratingselect: ratingselect
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .food
     position: fixed
     left: 0
@@ -133,7 +187,7 @@
           margin-right: 12px
       .price
         font-weight: 700
-        line-weight: 24px
+        line-height: 24px
         .now
           margin-right: 8px
           font-size: 14px
@@ -150,7 +204,7 @@
         position: absolute
         right: 18px
         bottom: 18px
-        z-index: 10px
+        z-index: 10
         height: 24px
         line-height: 24px
         padding: 0 12px
@@ -175,5 +229,49 @@
         line-height: 24px
         padding: 0 8px
         font-size: 12px
-        color: grb(77, 85, 93)
+        color: rgb(77, 85, 93)
+    .rating
+      padding-top: 18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .rating-wrapper
+        padding: 0 18px
+        .rating-item
+          position: relative
+          padding: 16px 0
+          border-1px(rgba(7, 17, 27, 0.1))
+          .user
+            position: absolute
+            right: 0
+            top: 16px
+            line-height: 12px
+            font-size: 0
+            .name
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+              font-size: 10px
+              color: rgb(147, 153, 159)
+            .avatar
+              border-radius: 50%
+          .time
+            margin-bottom: 6px
+            line-height: 12px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .text
+            line-height: 16px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+            .icon-thumb_up, .icon-thumb_down
+              margin-right: 4px
+              line-height: 16px
+              font-size: 12px
+            .icon-thumb_up
+              color: rgb(0, 160, 220)
+            .icon-thumb_down
+              color: rgb(147, 153, 159)
 </style>
