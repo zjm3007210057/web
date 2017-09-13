998fc67a9190643d3373e6c2d7b9b8995cc8543a
zjm <zjm3007210057@163.com>
2017-09-13 22:35:09 +0800

    add seller

--- elem-1.0/src/components/ratings/ratings.vue
+++ elem-1.0/src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
-<template>
-  <div class="ratings">
+<template xmlns:v-el="http://www.w3.org/1999/xhtml">
+  <div class="ratings" v-el:ratings>
     <div class="rating-content">
       <div class="overview">
         <div class="overview-left">
@@ -26,17 +26,44 @@
       </div>
       <split></split>
       <ratingselect :select-type="selectType" :only-content="onlyContent"
-                    :desc="desc" :ratings="food.ratings"></ratingselect>
+                    :desc="desc" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="rating in ratings" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
   import star from 'components/star/star'
   import split from 'components/split/split'
   import ratingselect from 'components/ratingselect/ratingselect'
+  import {formatDate} from 'common/js/date'
 
   const ALL = 2
+  const ERR_OK = 0
 
   export default {
     props: {
@@ -46,9 +73,55 @@
     },
     data() {
       return {
+        ratings: [],
         showFlag: false,
         selectType: ALL,
         onlyContent: true
+      }
+    },
+    created () {
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$els.ratings, {
+              click: true
+            })
+          })
+        }
+      })
+    },
+    methods: {
+      needShow(type, text) {
+        if(this.onlyContent && !text) {
+          return false
+        }
+        if(this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType
+        }
+      }
+    },
+    events: {
+      'ratingtype.select'(type) {
+        this.selectType = type
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd HH:mm')
       }
     },
     components: {
@@ -61,6 +134,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" res="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .ratings
     position: absolute
     top: 174px
@@ -127,4 +201,64 @@
             margin-left: 12px
             font-size: 12px
             color: rgb(147, 153, 159)
+    .rating-wrapper
+      padding: 0 18px
+      .rating-item
+        display: flex
+        padding: 18px 0
+        border-1px(rgba(7, 17, 27, 0.1))
+        .avatar
+          flex: 0 0 28px
+          width: 28px
+          margin-right: 12px
+          .img
+            border-radius: 50%
+        .content
+          position: relative
+          flex: 1
+          .name
+            margin-bottom: 4px
+            line-height: 12px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+          .star-wrapper
+            margin-bottom: 6px
+            font-size: 0
+            .star
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+            .delivery
+              display: inline-block
+              vertical-align: top
+              line-height: 12px
+              font-size: 10px
+              color: rgb(147, 152, 159)
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            color: rgb(7, 17, 27)
+            font-szie: 12px
+          .recommend
+            line-height: 16px
+            font-size: 0
+            .icon-thumb_up, .item
+              display: inline-block
+              margin: 0 8px 4px 0
+              font-size: 9px
+            .icon-thumb_up
+              coclor: rgb(0, 160, 220)
+            .item
+              padding: 0 6px
+              border: 1px solid rgba(7, 17, 27, 0.1)
+              border-redius: 1px
+              color: rgb(147, 150, 159)
+              background: #fff
+          .time
+            position: absolute
+            top: 0
+            right: 0
+            line-height: 12px
+            font-size: 10px
+            color: rgb(147, 150, 159)
 </style>
