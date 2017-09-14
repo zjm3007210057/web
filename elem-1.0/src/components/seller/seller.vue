d918a3086a553eba40aea472fc83f0c4ae499ed9
JianmingZhang <zhang.jianming@opg.cn>
2017-09-14 12:19:58 +0800

    add seller

--- elem-1.0/src/components/seller/seller.vue
+++ elem-1.0/src/components/seller/seller.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-el="http://www.w3.org/1999/xhtml">
   <div class="seller">
     <div class="seller-content">
       <div class="overview">
@@ -28,19 +28,48 @@
             </div>
           </li>
         </ul>
+        <div class="favorite">
+          <span class="icon-favorite" :class="{'active':favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
       </div>
       <split></split>
       <div class="bulletin">
         <h1 class="title">公共与活动</h1>
-        <div class="content-wrapper">
+        <div class="content-wrapper border-1px">
           <p class="content">{{seller.bulletin}}</p>
         </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="item in seller.supports">
+            <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+            <span class="text">{{seller.supports[$index].description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" v-el:pic-wrapper>
+          <ul class="pic-list" v-el:pic-list>
+            <li class="pic-item" v-for="pic in seller.pics">
+              <img :src="pic" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="info in seller.infos">{{info}}</li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
   import star from 'components/star/star'
   import split from 'components/split/split'
 
@@ -48,6 +77,42 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    created() {
+      this.classMap = ['descrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    ready() {
+      this._initScroll()
+      this._initPics()
+    },
+    methods: {
+      _initScroll() {
+        if(!this.scroll) {
+          this.scroll = new BScroll(this.$els.seller, {
+            click: true
+          })
+        }else {
+          this.scroll.refresh()
+        }
+      },
+      _initPics() {
+        if(this.seller.pics) {
+          let picWidth = 120
+          let margin = 6
+          let width = (picWidth + margin) * this.seller.pics.length - margin
+          this.$els.picList.style.width = width + 'px'
+          this.$nextTick(() => {
+            if(!this.picScroll) {
+              this.picScroll = new BScroll(this.$els.picWrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              })
+            }else {
+              this.picScroll.refresh()
+            }
+          })
+        }
       }
     },
     components: {
@@ -131,4 +196,26 @@
           font-size: 0
           &:last-child
             border-none()
+        .icon
+          display: inline-block
+          width: 16px
+          height: 16px
+          vertical-align: top
+          margin-right: 6px
+          background-size: 16px 16px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_4')
+          &.discount
+            bg-image('discount_4')
+          &.guarantee
+            bg-image('guarantee_4')
+          &.invoice
+            bg-image('invoice_4')
+          &.special
+            bg-image('special_4')
+        .text
+          line-height: 16px
+          font-size: 12px
+          color: rgb(7, 17, 27)
 </style>
